43aa6fbe80941ebc1a5c6882b0916f27b521ddb9
Yufan Wang <yufansmail@gmail.com>
2018-01-17 15:28:23 -0800

    FUNCTIONAL STACK
    passed all tests

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -4,14 +4,25 @@ var Stack = function() {
   // Use an object with numeric keys to store values
   var storage = {};
 
+  var counter = 0;
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[counter] = value;
+    counter++;
   };
 
   someInstance.pop = function() {
+    counter--;
+    let val = storage[counter];
+    delete storage[counter];
+    return val;
   };
 
   someInstance.size = function() {
+    if(counter < 0) {
+      return 0;
+    }
+    return counter;
   };
 
   return someInstance;
