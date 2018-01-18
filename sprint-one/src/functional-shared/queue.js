7f68cb40d11f5b8fa9b8bedecead446f26ab9fba
Yufan Wang <yufansmail@gmail.com>
2018-01-17 16:01:25 -0800

    FUNCTIONAL SHARED QUEUE
    passed all tests

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,35 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var instance = {};
+
+  instance.storage = {};
+  instance.min = 0;
+  instance.max = 0;
+
+  _.extend(instance, queueMethods);
+
+  return instance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function (val) {
+    this.storage[this.max] = val;
+    this.max++;
+  },
 
+  dequeue: function () {
+    let val = this.storage[this.min];
+    delete this.storage[this.min];
+    this.min++;
+    return val;
+  },
+
+  size: function () {
+    if(this.max - this.min < 0) {
+      return 0;
+    }
+    return this.max - this.min;
+  }
+};
 
