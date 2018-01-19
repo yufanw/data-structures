fcb4d8d632a0c0abbab0e6d4818174d0aa07582e
Yufan Wang <yufansmail@gmail.com>
2018-01-18 19:01:31 -0800

    LINKED LISTS
    passed all tests

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,14 +4,35 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    if (!list.head) {
+      list.tail = Node(value);
+      list.head = list.tail;
+    } else {
+      var currentNode = list.head;
+      while (currentNode.next) {
+        currentNode = currentNode.next;
+      }
+      currentNode.next = Node(value);
+      list.tail = currentNode.next;
+    }
   };
 
   list.removeHead = function() {
+    var removedHead = list.head.value;
+    list.head = list.head.next;
+    return removedHead;
   };
 
   list.contains = function(target) {
+    var currentNode = list.head;
+    while (currentNode) {
+      if (currentNode.value === target) {
+        return true;
+      }
+      currentNode = currentNode.next;
+    }
+    return false;
   };
-
   return list;
 };
 
@@ -27,3 +48,7 @@ var Node = function(value) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+//addToTail: O(n)
+//removeHead: O(1)
+//contains: O(n)
+
