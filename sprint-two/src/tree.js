aaef83fece1b242e0e0801c7f93498ae1e52d6d1
Yufan Wang <yufansmail@gmail.com>
2018-01-18 21:29:54 -0800

    TREE
    passed all tests

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,9 +1,9 @@
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
+  newTree.children = [];
 
-  // your code here
-  newTree.children = null;  // fix me
+  _.extend(newTree, treeMethods);
 
   return newTree;
 };
@@ -11,13 +11,43 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var child = Tree(value);
+  this.children.push(child);
 };
 
 treeMethods.contains = function(target) {
+  var doesContain = false;
+  if (this.value === target) {
+    doesContain = true;
+  }
+  for (var i = 0; i < this.children.length; i++) {
+    if (!doesContain) {
+    doesContain = this.children[i].contains(target);
+    }
+  }
+  return doesContain;
 };
-
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+//addChild: O(1)
+//contains: O(n)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
