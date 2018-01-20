9ef7d6ceb17c9a234b670c0de76a1e126541f6ec
Yufan Wang <yufansmail@gmail.com>
2018-01-19 16:41:11 -0800

    HashTable
    implemented functions, passes all tests except overwriting properties

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,16 +7,40 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  if(this._storage.get(index)) {
+    this._storage.each(function(el, i, storage) {
+      if (storage[index][i][0] === k) {
+        this._storage.set(index,[[k, v]]);
+      } else {
+        storage[index].push([k, v]);
+      }
+    });
+  } else {
+    this._storage.set(index,[[k, v]]);
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+
+  if (this._storage.get(index)) {
+    var tuples = this._storage.get(index);
+    for (let i = 0; i < tuples.length; i++) {
+      if (tuples[i][0] === k) {
+        return tuples[i][1];
+      }
+    }
+  }
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.each(function(el, i, storage) {
+    if (i === index) {
+      storage.splice(i, 1);
+    }
+  });
 };
-
 
 
 /*
